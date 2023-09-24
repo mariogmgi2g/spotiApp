525b4c7469d83d8cebfa71cafab398943859072e
Marina Bruno Arias <marinaba98@gmail.com>
2023-09-24 11:00:18 +0200

    clase 24/09

--- src/main.py
+++ src/main.py
@@ -5,22 +5,23 @@ import pandas as pd
 id_playlist = '37i9dQZF1DWWGFQLoP9qlv'
 querier = SpotyParser()
 
-respuesta1 = querier.query('top n artists', 10)
-respuesta2 = list(querier.query('top n genres', 10))
-respuesta3 = querier.query('top n tracks', 10)
-respuesta4 = querier.query('top n tracks artists', 10)
-respuesta5 = list(querier.query('playlist tracks', id_playlist).keys())
-respuesta6 = querier.query('track features from playlist', id_playlist)
+querier.query('playlist followers', id_playlist)
+# respuesta1 = querier.query('top n artists', 10)
+# respuesta2 = list(querier.query('top n genres', 10))
+# respuesta3 = querier.query('top n tracks', 10)
+# respuesta4 = querier.query('top n tracks artists', 10)
+# respuesta5 = list(querier.query('playlist tracks', id_playlist).keys())
+# respuesta6 = querier.query('track features from playlist', id_playlist)
 
-data = {
-    'top 10 artists': respuesta1,
-    'top genres': respuesta2, 
-    'top 10 tracks': respuesta3, 
-    'top tracks artists': respuesta4, 
-    'playlist tracks ' + id_playlist: respuesta5, 
-    'track features from playlist': respuesta6, 
-}
-print(data)
+# data = {
+#     'top 10 artists': respuesta1,
+#     'top genres': respuesta2, 
+#     'top 10 tracks': respuesta3, 
+#     'top tracks artists': respuesta4, 
+#     'playlist tracks ' + id_playlist: respuesta5, 
+#     'track features from playlist': respuesta6, 
+# }
+#print(data)
 with open('./data.json', 'w') as fp:
     json.dump(data, fp)
 
--- src/spoty_parser.py
+++ src/spoty_parser.py
@@ -3,6 +3,8 @@ from auth import Auth
 import json
 import numpy as np
 import pandas as pd
+import random
+import urllib.request
 
 # Posibles instrucciones = 
 #   -> top n artists
@@ -26,7 +28,7 @@ class SpotyParser:
     def query(self, query_key:str, *args) -> dict:
         implemented_query_keys = [
             'top n artists', 'top n genres', 'top n tracks', 
-            'top n tracks artists', 'playlist tracks', 'track features',
+            'top n tracks artists', 'playlist cover image', 'playlist followers','playlist tracks', 'track features',
             'track features from playlist']
 
         if query_key == 'top n artists':
@@ -45,6 +47,20 @@ class SpotyParser:
             (url_char, params) = SpotyParser.__query_top_tracks(args[0], 0)
             response = self.__url_builder(url_char, params)
             response = SpotyParser.__get_top_tracks_artists(response)
+
+        elif query_key == 'playlist cover image':
+            (url_char, params) = SpotyParser.__get_playlist_image(args[0])
+            response = self.__url_builder(url_char, params)
+            print(response)
+            self.__download_playlist_image(response)
+
+        elif query_key == 'playlist followers':
+            (url_char, params) = SpotyParser.__get_playlist_followers(args[0])
+            response = self.__url_builder(url_char, params)
+            response = self.__followers(response)
+            print(response)
+
+
         elif query_key == 'playlist tracks':
             (url_char, params) = SpotyParser.__query_playlist_tracks(args[0])
             response = self.__url_builder(url_char, params)
@@ -82,7 +98,7 @@ class SpotyParser:
         return response
 
     # --------------------------------------------------------------------------
-    def __query_top_artists(n:int, offset:int=5) -> tuple:
+    def __query_top_artists(n:int, offset:int=0) -> tuple:
         url_char = "me/top/artists"
 
         params = {
@@ -138,6 +154,26 @@ class SpotyParser:
 
     # --------------------------------------------------------------------------
 
+    def __get_playlist_image(id_playlist:str):
+        url_char = "playlists/" + id_playlist + "/images"
+        params = {
+            'time_range' : 'medium_term',
+            'limit' : 10,
+            'offset' : 0
+        }
+        return (url_char, params)
+
+    def __download_playlist_image(self, response):
+        image_url = response[0]['url']
+        SpotyParser.download_image(image_url)
+
+    def download_image(url):
+        name = r'c:\Users\marin\OneDrive\Escritorio\CURSO_PHYTON\02_EPPY\act_02\spotiApp\cover'
+        fullname = str(name)+".jpg"
+        urllib.request.urlretrieve(url,fullname)
+        
+
+
     def __query_playlist_tracks(id_playlist:str) -> tuple:
         #37i9dQZF1DWWGFQLoP9qlv
         url_char = "playlists/" + id_playlist
@@ -148,6 +184,19 @@ class SpotyParser:
             'offset' : 0
         }
         return (url_char, params)
+    
+    def __get_playlist_followers(id_playlist:str):
+        url_char = "playlists/" + id_playlist
+        params = {
+            'time_range' : 'medium_term',
+            'limit' : 10,
+            'offset' : 0
+        }
+        return (url_char, params)
+    
+    def __followers(self, response):
+        response['followers']
+        print(response)
     
 
     def __query_track_features(id_track:str) -> tuple:
